426ba84f7f69babe76f6eb6880c122e875da9c70
Andreas Guther <andreas@guther.net>
2016-04-23 23:37:30 +0200

    RPMS for various JetBrains IntelliJ Plugins

---
+++ invoke.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# get directories in current directory
+for directory in $(find . -maxdepth 1 -mindepth 1 -type d | sort)
+do
+	# get basename from directory
+	directory=$(basename "${directory}")
+
+	# inform user about current status
+	echo
+	echo -------------------------------------------------------------------------------
+	echo Processing \'${directory}\' ...
+	echo -------------------------------------------------------------------------------
+	echo
+
+	# change into directory
+	pushd "${directory}" 1>/dev/null 2>&1
+
+	# echo current dir
+	eval "$@"
+
+	# restore
+	popd 1>/dev/null 2>&1
+done
